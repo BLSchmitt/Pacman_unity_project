a3c3263366d89d9eb01334c30bd196398e39a62a
BLSchmitt <30492877+BLSchmitt@users.noreply.github.com>
2019-06-04 15:39:42 -0400

    First player deplacement
    Le joueur peut se déplacer sans traverser les murs.

--- Pacman_Benoît_Schmitt/Assets/Scripts/PlayerController.cs
+++ Pacman_Benoît_Schmitt/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public Rigidbody rb;
+    public float speed;
+
+    private string movementAxeHorizontal;
+    private string movementAxeVertical;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        movementAxeHorizontal = "Horizontal";
+        movementAxeVertical = "Vertical";
+        speed = 1f;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float movementHorizontal = Input.GetAxis(movementAxeHorizontal);
+        float movementVertical = Input.GetAxis(movementAxeVertical);
+        Vector3 movement = new Vector3(movementHorizontal,0.0f,movementVertical);
+
+        rb.MovePosition(transform.position + movement*speed);
+        //rb.AddForce(movement*speed);
+
+    
     }
 }
