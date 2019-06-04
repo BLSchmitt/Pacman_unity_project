7ad7e0f8c793497516df59aef6e2f900a9301a68
BLSchmitt <30492877+BLSchmitt@users.noreply.github.com>
2019-06-04 16:14:01 -0400

    Ajout du premier pickup
    Ajout du premier objet à récupérer.
    Ajout du script pour interargir avec les éléments de la partie (pickup et ennemis)

---
+++ Pacman_Benoît_Schmitt/Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+   
+   void OnTriggerEnter(Collider coco){
+
+       string tag = coco.gameObject.tag;
+
+       // si on récupère un objet 
+       if(tag == "pickup"){
+           // on fait disparaitre l'objet
+
+           // on augmente le compte des objets
+
+
+           // si on les a tous alors on fini la partie
+       }
+        else{
+            // si on impact un ennemi
+            if(tag == "ennemis"){
+                // si on a encore une vie on recommence
+
+                // si on a plus de vie en stock la partie se termine
+
+            }
+        }
+
+
+   }
+}
