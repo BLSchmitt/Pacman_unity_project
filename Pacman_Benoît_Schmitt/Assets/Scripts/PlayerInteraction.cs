fa712cb83ffda8aa21fcd2b82f08496e852871f7
BLSchmitt <30492877+BLSchmitt@users.noreply.github.com>
2019-06-04 17:01:33 -0400

    Update PlayerInteraction.cs

--- Pacman_Benoît_Schmitt/Assets/Scripts/PlayerInteraction.cs
+++ Pacman_Benoît_Schmitt/Assets/Scripts/PlayerInteraction.cs
@@ -7,12 +7,14 @@ using UnityEngine.UI;
 public class PlayerInteraction : MonoBehaviour
 {
     public Text countPickupText;
-    public Text countLifeText;
+    public Text lifeText;
     public Text winningText;
     public Text loosingText;
     
     public int countPickup;
+    public int life;
     private int countMax;
+
 
 
 
@@ -20,29 +22,27 @@ public class PlayerInteraction : MonoBehaviour
     {
         countPickup = 0;
         countMax = 5;
-        countLifeText.text = "1 vie";
+        life = 1;
+        lifeText.text = life.ToString() + " vie";
         CountingMessage();
 
         // text en cas de fin de parti
-        winningText.text = "";
-        loosingText.text = "";
+        winningText.text = " ";
+        loosingText.text = " ";
 
 
     }
 
     void OnTriggerEnter(Collider coco){
-
        string tag = coco.gameObject.tag;
 
        // si on récupère un objet 
        if(tag == "pickup"){
            // on fait disparaitre l'objet
            coco.gameObject.SetActive(false);
-
            // on augmente le compte des objets
            countPickup++;
            CountingMessage();
-            
            // si on les a tous alors on fini la partie
            if(countPickup == countMax){
                 winningText.text = "Bien joué tu as gagné !";
@@ -52,11 +52,17 @@ public class PlayerInteraction : MonoBehaviour
         else{
             // si on impact un ennemi
             if(tag == "ennemis"){
+                // on perd la vie
+                life--;
+                lifeText.text = life.ToString() + " vie";
                 // si on a encore une vie on recommence
-
+                if(life >0){
+                    // on recommence du début
+                }
+                else{
                 // si on a plus de vie en stock la partie se termine
                 loosingText.text = "Dommage essay encore :D";
-
+                }
             }
         }
 
@@ -64,7 +70,7 @@ public class PlayerInteraction : MonoBehaviour
 
     void CountingMessage()
     {
-        countPickupText.text = "restant : " +(countMax-countPickup).ToString();
+        countPickupText.text = "    restant : " +(countMax-countPickup).ToString();
     }
 
 
