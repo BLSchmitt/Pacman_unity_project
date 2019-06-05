815222bf0ca7b67754dceb036db204ac87e87b41
BLSchmitt <30492877+BLSchmitt@users.noreply.github.com>
2019-06-05 17:07:11 -0400

    PowerUp
    Ajout du PowerUp qui permet de manger les ennemis.
    Les ennemis changent de couleur pendant un certain temps, ils sont alors dévorable par le jouer.
    Reste à implementer de les faire revire.

---
+++ Pacman_Benoît_Schmitt/Assets/Scripts/EnnemisInteraction.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemisInteraction : MonoBehaviour
+// ce script gere toutes les interactions avec les ennemis
+{
+    public GameObject[] ennemis; 
+    public Renderer[] rend;
+    public Color[] color;
+    
+    public int numberOfEnnemis;
+    // initialisation
+    void Awake(){
+        numberOfEnnemis = ennemis.Length;
+        rend = new Renderer[numberOfEnnemis];
+        color = new Color[numberOfEnnemis];
+        for (int i=0;i<numberOfEnnemis;i++){
+            rend[i] = ennemis[i].GetComponent<Renderer>();
+            color[i] = rend[i].material.color;
+        }
+    }
+
+
+
+    // ennemis se fait manger, appelle par PlayerInteraction
+    // quand powerUp est active et suite a une collision player / ennemis
+    public void ennemisDestruction(Collider coco){
+        coco.gameObject.SetActive(false);
+  }
+    // appelle par PlayerInteraction quand on mange un powerup
+    // le player peut alors manger ses ennemis pend un certain temps
+    // les ennemis deviennent bleu
+  public void ennemisAreKillable(){
+    for(int i=0;i<numberOfEnnemis;i++){
+          rend[i].material.color = Color.blue;
+    }
+  }
+
+    // appelle par PlayerInteraction quand le powerup se finit 
+    // le joueur ne peut plus manger ses ennemis
+    // les ennemis reprennent leurs couleurs d'origine
+    public void ennemisAreNotKillable(){
+        for(int i=0; i<numberOfEnnemis;i++){
+            rend[i].material.color = color[i];
+        }
+    }
+
+}
--- Pacman_Benoît_Schmitt/Assets/Scripts/PlayerInteraction.cs
+++ Pacman_Benoît_Schmitt/Assets/Scripts/PlayerInteraction.cs
@@ -4,8 +4,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class PlayerInteraction : MonoBehaviour
 {
+
+    public GameObject ennemis;
+    // la class controllant les interactions ennemis
+    private EnnemisInteraction ennemisInteraction;
     // text comptant le nombre restant de pickup à recolter
     public Text countPickupText;
     // text comptant les vies restantes
@@ -16,16 +21,21 @@ public class PlayerInteraction : MonoBehaviour
     public Text loosingText;
     
     // les nombres de pickup récupéré  
-    public int countPickup;
+    private int countPickup;
     // le nombre de vie restante
     public int life;
     // le nombre de pickup total
     public int countMax = 54;
-    
+
     // si on a recuperer le powerUp qui permet de manger les fantomes
-    public Boolean powerUp;
-
-
+    private bool powerUp;
+    // la duree du powerUp en seconde
+    public float tempsDePowerUp = 2f;
+    
+    void Awake()
+    {
+        ennemisInteraction = ennemis.GetComponent<EnnemisInteraction>();
+    }
     void Start()
     {
         // initialisation des variables
@@ -44,20 +54,27 @@ public class PlayerInteraction : MonoBehaviour
     void OnTriggerEnter(Collider coco)
     {
        string tag = coco.gameObject.tag;
-       // si on récupère un objet 
+       // si on récupère un objet pickup
        if(tag == "pickup"){
-          Pickingup(coco);
+          PickupCollecting(coco);
        }
-        else
-        {
-            // si on impact un ennemi
-            if(tag == "ennemis"){
-                EnnemisImpacting(coco);
+        else{
+            // si on recupere un objet powerup
+            if(tag == "powerup"){
+                    PowerupCollecting(coco);
+                }
+                else
+                {
+                    // si on impact un ennemi
+                    if(tag == "ennemis"){
+                        EnnemisImpacting(coco);
+                }
             }
         }
    }
 
-    private void Pickingup(Collider coco){
+
+    private void PickupCollecting(Collider coco){
         // on fait disparaitre l'objet
         coco.gameObject.SetActive(false);
         // on augmente le compte des objets
@@ -70,10 +87,32 @@ public class PlayerInteraction : MonoBehaviour
         }
     }
 
-    private void EnnemisImpacting(Collider coco){
+    private void PowerupCollecting(Collider coco)
+    {
+        // c'est aussi un pickup
+        PickupCollecting(coco);
+        // on peut manger les ennemis pendant un certain temps
+        powerUp = true;
+        ennemisInteraction.ennemisAreKillable();
+        // apres un certain temps on ne peut plus les manger
+        StartCoroutine(powerUpActif());
+    }
+
+    IEnumerator powerUpActif()
+    {
+        // apres t seconde on ne peut plus manger les ennemis
+        yield return new WaitForSeconds(tempsDePowerUp);
+        powerUp = false;
+        ennemisInteraction.ennemisAreNotKillable();
+    }
+
+    // appelle lors d un contact entre le joueur et un ennemis
+    public void EnnemisImpacting(Collider coco){
         // si on a le powerup actif alors on mange les fantomes
         if(powerUp){
-
+            // détruit le fantome
+            // le fantome rentre a sa position initial et reaparait de labas
+            ennemisInteraction.ennemisDestruction(coco);
         }
         //sinon on se fait manger
         else{
@@ -83,7 +122,7 @@ public class PlayerInteraction : MonoBehaviour
             // si on a encore une vie on recommence
             if(life >0){
                 // on recommence du début
-                
+                restartLvl();
             }
             else{
             // si on a plus de vie en stock la partie se termine
@@ -93,6 +132,12 @@ public class PlayerInteraction : MonoBehaviour
             }
         }
     }
+
+    private void restartLvl()
+    {
+        throw new NotImplementedException();
+    }
+
 
     // on quitte la partie apres 2 secondes
     IEnumerator theEnd(){
